f98b28b711d3f9ef544a72a5ae018d8b94bb4f4f
Jonibek <jonibeknorboev@gmail.com>
2020-02-20 17:57:00 +0900

    Second Commit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,30 +4,61 @@ import ReactAux from '../../hoc/ReactAux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRIZES = {
+  salad: 0.5,
+  bacon: 0.5,
+  cheese: 1.3,
+  meat: 0.7,
+}
+
 class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2,
-    }
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+    totalPrize: 4,
   }
 
   addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCount;
 
+    const oldPrize = this.state.totalPrize;
+    const newPrize = oldPrize + INGREDIENT_PRIZES[type];
+    this.setState({ingredients: updatedIngredients, totalPrize: newPrize});
   }
 
   removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if(oldCount === 0) return;
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCount;
 
+    const oldPrize = this.state.totalPrize;
+    const newPrize = oldPrize - INGREDIENT_PRIZES[type];
+    this.setState({ingredients: updatedIngredients, totalPrize: newPrize});
   }
 
   render () {
+
+    const disabledInfo = {
+      ...this.state.ingredients
+    }
+
     return (
       <ReactAux>
         <Burger ingredients={this.state.ingredients}/>
-        <BuildControls />
+        <BuildControls
+          ingredientAdded = {this.addIngredientHandler}
+          ingredientRemoved = {this.removeIngredientHandler}
+          />
       </ReactAux>
     )
   }
